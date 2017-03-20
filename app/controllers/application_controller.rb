c9354622b748ae175d1fa66cd856845f91700f58
koyukato <rock.tiwlight728@gmail.com>
2017-03-20 23:15:47 +0900

    Hello world!!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    name = "torinist"
+    render text: "Hello, #{name}, Welcome to Ruby!"
+  end
+
 end
