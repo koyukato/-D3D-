94659bfdca8ee8a792be1e2509237f3c3d975d80
NaokiSatou <yomicerisier@gmail.com>
2017-03-20 18:42:29 +0900

    rails sできない問題の修正

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,13 @@ module Taeko
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+    g.javascripts false
+    g.stylesheets false
+    g.template_engine :haml
+    g.test_framework :rspec, view_specs: false, fixture: true
+    g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
 
-config.generators do |g|
-  g.javascripts false
-  g.stylesheets false
-  g.template_engine :haml
-  g.test_framework :rspec, view_specs: false, fixture: true
-  g.fixture_replacement :factory_girl, dir: 'spec/factories'
-end
