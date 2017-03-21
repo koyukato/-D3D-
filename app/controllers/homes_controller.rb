e43b32ff5796a212a5eba266b72309d5b8a84d13
koyukato <rock.tiwlight728@gmail.com>
2017-03-21 23:35:41 +0900

    jsonのデータをテーブルで表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,11 +7,6 @@ class HomesController < ApplicationController
 		res = Net::HTTP.get(URI.parse('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20121121?applicationId=1012194014463186177&categoryType=medium'))
 
 		@result = JSON.parse(res)
-		#puts res.to_s
-		(1..9).each do|i|
-			@msg=@result['result']['medium'][i].to_s
-			#@msg=result['result']['large'][0]['categoryName'].to_s
-		end
 	end
 
 
