cfbfdf082f1acb155fc8624296f0f783ebf0d4ca
yu <louscania@icloud.com>
2017-04-18 15:14:43 +0900

    cssの細分化と全体cssの作成

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( list/kute.js )
 Rails.application.config.assets.precompile += %w( list/kute.min.js )
 Rails.application.config.assets.precompile += %w( list/kute.scss )
+Rails.application.config.assets.precompile += %w( meinstyle/mein.scss )
